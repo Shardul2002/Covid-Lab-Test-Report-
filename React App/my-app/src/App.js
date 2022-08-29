addc1dbaa75eabe45373471a147c2cbdf02d9e13
Shardul Joshi <sharduljoshi9@gmail.com>
2022-08-29 17:44:57 +0530

    Notes-Maker App
    Notes-Maker App

---
+++ React App/my-app/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {BrowserRouter as Router,Routes,Link} from 'react-router-dom';
+import Header  from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import CreateArea from './CreateArea';
+
+
+
+
+function App() {
+  const[notes,setNotes]=useState([]);
+
+  function addNote(note){
+      setNotes(prevNotes => { 
+        return [...prevNotes,note];
+      });
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes =>{
+      return prevNotes.filter((noteItem,index)=>{
+        return index!==id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((noteItem,index) => {
+        return (
+          <Note key={index} 
+          id={index} 
+          title={noteItem.title} 
+          content={noteItem.content} 
+          onDelete={deleteNote}
+           />
+           );
+      })}
+     
+      <Footer />
+    
+    
+  
+    
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ React App/my-app/src/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function profile(){
+    return (
+    <div>
+        <h1>This is profile page</h1>
+    </div>
+    );
+
+}
+
+export default profile;
